dc00fb546df1a19ada513873b1ed34f3e20e7deb
hhennessyy <juandavidytm1@gmail.com>
2024-01-28 21:19:33 -0500

    feat: :tada: Creacion del proyecto para consumir la API de SpaceX
    Generacion del proyecto SpaceX por medio de Angular CLI, y configuracion de la integracion de Tailwind CSS y Flowbite Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { HubComponent } from './pages/hub/hub.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HubComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SpaceX';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
